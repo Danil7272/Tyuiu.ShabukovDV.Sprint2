f3a9027105ff9014da765e1a04a5f45c05e2705b
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-15 21:40:46 +0400

    Task4

---
+++ Tyuiu.ShabukovDV.Sprint2.Task4.V30.Lib/ISprint2Task4V30.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.ShabukovDV.Sprint2.Task4.V30.Lib
+{
+    public interface ISprint2Task4V30
+    {
+    }
+}
---
+++ Tyuiu.ShabukovDV.Sprint2.Task4.V30/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShabukovDV.Sprint2.Task4.V30.Lib;
+
+namespace Tyuiu.ShabukovDV.Sprint2.Task4.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Шабуков Д. В. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Шабуков Данил Валерьевич  | СМАРТб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на, которая вычисляет требуемое значение с           *");
+            Console.WriteLine("* с использованием тенарного оператора где пользователь вводит значения   *");
+            Console.WriteLine("* переменных с клавиатуры                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x, y;
+            Console.WriteLine("Введите значение переменной X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.Calculate(x, y);
+            Console.WriteLine(Math.Round(res, 3));
+            Console.ReadKey();
+        }
+    }
+}
